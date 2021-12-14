29f3a3da34b401fa08f82cde3df6644f27d31e70
elnurxo <elnurykh@code.edu.az>
2021-12-15 00:50:42 +0400

    front final first commit,header,footer,loader

---
+++ FrontEndFinalElnurP124/assets/Js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+    // Loader
+    // $(window).on("load",function(){
+    //     setTimeout(removeLoader,1000);
+    //     $('#loadingwrapper').css({"display":"block","visibility":"visible","opacity":"1"});
+    // })
+    // function removeLoader(){
+    //     $('#loadingwrapper').fadeOut(500,function(){
+    //         $("#loadingwrapper").remove();
+    //     })
+    // }
+
+});
